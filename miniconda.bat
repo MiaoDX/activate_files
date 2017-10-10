867ab24608b7d10b39e935602242dcfbf9313c79
MiaoDX <miaodx@hotmail.com>
2017-10-10 14:40:42 +0800

    Add %HOMEPATH% and %py_env_conda% variable.

--- miniconda.bat
+++ miniconda.bat
@@ -1,4 +1,3 @@
-set Conda3_DIR=C:\Users\miao\Miniconda3
-
+set Conda3_DIR=%HOMEPATH%\Miniconda3
 
 set path=%path%;%Conda3_DIR%;%Conda3_DIR%\Scripts;%Conda3_DIR%\Library\bin;
--- python35.bat
+++ python35.bat
@@ -1,3 +1,3 @@
-set Python_DIR=%py_env_conda%\py_36_bare
+set Python_DIR=%py_env_conda%\py_35_bare
 
 set path=%path%;%Python_DIR%;%Python_DIR%\Scripts;%Python_DIR%\Library\bin;
