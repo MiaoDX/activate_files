4ce7f1c42a8bcedc9714d63f80bd95637376431f
DongxuMiao <dongxumiao@deepmotion.ai>
2019-11-12 12:15:12 +0800

    update mount remote pyenv

--- linux/mount_remote_pyenv.sh
+++ linux/mount_remote_pyenv.sh
@@ -1,9 +1,10 @@
 conda_dir=/mnt/remote_server/<conda_path>
 env=<env_name>
 
-#conda_dir=/home/dongxu/dm6_mdx/home/zhixiang/miniconda3
+#conda_dir=/mnt/sda/home/zhixiang/dongxu/mnt/dm6/home/zhixiang/miniconda3
 #env_name=py37t11
 
-source ${conda_dir}/bin/activate
-source activate ${conda_dir}/envs/${env_name}
+source ${PWD}/cp_conda.sh $conda_dir/bin/python
+#conda activate ${conda_dir}/envs/${env_name}
+conda activate ${env_name}
 
